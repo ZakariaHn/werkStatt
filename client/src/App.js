5d1bfa4d63117ee503c0cf69884e11ef422b9522
zak258 <zakaria.k.hamdan@gmail.com>
2020-10-27 20:53:12 +0100

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { ToggleLists } from "./components/ToggleLists";
 import { ComponentsHolder } from "./components/content/ComponentsHolder";
+import { Footer } from "./components/footer";
+import { Navbar } from "./components/NavBar";
 
 const App = () => {
   return (
     <div className="container">
-      <ToggleLists />
+      <Navbar />
       <ComponentsHolder />
+      <Footer />
     </div>
   );
 };
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,44 @@
 import React from "react";
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  faSearch,
+  faCog,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { faLaravel } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => {
   return (
-    <div>
-      <AppBar position="static">
-        <Tabs>
-          <Tab label="Clients" onChange={() => <Redirect to="/clients"></Redirect>}>
-
-          </Tab>
-          <Tab label="Cars" onChange={() => <Redirect to="/cars"></Redirect>}>
-            <Link to="/cars">Cars</Link>
-          </Tab>
-          <Tab label="Operations">
-            <Link to="/operations">Operations</Link>
-          </Tab>
-        </Tabs>
-      </AppBar>
+    <div className="navBar">
+      <div className="logo">
+        <FontAwesomeIcon icon={faLaravel} />
+      </div>
+      <div className="all">
+        <div className="searchBarHolder">
+          <input placeholder="Search for a client, car or an operaiton"></input>
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+        <div className="tapsAndSetting">
+          <div className="taps">
+            <div>
+              <Link to="/clients">Clients</Link>
+            </div>
+            <div>
+              <Link to="/cars">Cars</Link>
+            </div>
+            <div>
+              <Link to="/operations">Operations</Link>
+            </div>
+          </div>
+          <div className="setting">
+            <FontAwesomeIcon icon={faUserCircle} />
+            <FontAwesomeIcon icon={faCog} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
