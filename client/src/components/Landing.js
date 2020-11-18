c90cfae0676b65f77582d153c011afa4735cbbba
zak258 <zakaria.k.hamdan@gmail.com>
2020-11-18 20:32:02 +0100

    forms tranistions

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { loginAction } from "../store/actions/authActions";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faLaravel } from "@fortawesome/free-brands-svg-icons";
 import { Footer } from "./footer";
-import { CLICKED, TAB_CLICKED } from "../store/actions/types";
+import { CLICKED } from "../store/actions/types";
+import { motion } from "framer-motion";
 
 export const Landing = () => {
+  const [signUpTransition] = useState({
+    pageVariants: {
+      initial: {
+        opacity: 0,
+        x: "-100vw",
+        scale: 0.8,
+      },
+      in: {
+        opacity: 1,
+        x: 0,
+        scale: 1,
+      },
+      out: {
+        opacity: 0,
+        x: "100vw",
+        scale: 1.2,
+      },
+    },
+    pageTransition: {
+      type: "tween",
+      ease: "anticipate",
+      duration: 1,
+    },
+    pageStyle: {
+      position: "absolute",
+    },
+  });
+  const { pageStyle, pageVariants, pageTransition } = signUpTransition;
   const dispatch = useDispatch();
 
   const { register, handleSubmit, errors } = useForm();
@@ -83,7 +112,15 @@ export const Landing = () => {
   };
 
   const signUpForm = () => (
-    <div className="login-form">
+    <motion.div
+      style={pageStyle}
+      initial="out"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+      className="login-form"
+    >
       <strong>PLEASE SIGN UP</strong>
       <form onSubmit={handleSubmit(onSignIn)}>
         <div>
@@ -114,15 +151,24 @@ export const Landing = () => {
         </div>
         <input className="submit-button" type="submit" value="SIGN UP" />
       </form>
-    </div>
+      <div
+        className="back"
+        onClick={() => dispatch({ type: CLICKED, payload: "" })}
+      >
+        {"<"}
+      </div>
+    </motion.div>
   );
+
   const isClicked = useSelector((state) => state.target.isClicked);
 
   return (
     <div className="landing-page">
       {landingPageNavBar()}
-      {loginHeader()}
-      {isClicked === "signUpClicked" ? signUpForm() : loginForm()}
+      <div className="landing-content">
+        {loginHeader()}
+        {isClicked === "signUpClicked" ? signUpForm() : loginForm()}
+      </div>
       <Footer />
     </div>
   );
