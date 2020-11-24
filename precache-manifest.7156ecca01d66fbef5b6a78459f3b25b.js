841b4252639499b45e93159a61e5ebb934cb0b62
zak258 <zakaria.k.hamdan@gmail.com>
2020-11-24 17:29:03 +0100

    Updates

---
+++ precache-manifest.7156ecca01d66fbef5b6a78459f3b25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52708a1fd82ce05de978252b7243bd47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c95fd7993878c56db9f6",
+    "url": "/static/css/main.ee05b627.chunk.css"
+  },
+  {
+    "revision": "d0a46cbfc051cd06cfbf",
+    "url": "/static/js/2.f8ea0315.chunk.js"
+  },
+  {
+    "revision": "80dd593b1b521fb1d672c186b43af21e",
+    "url": "/static/js/2.f8ea0315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95fd7993878c56db9f6",
+    "url": "/static/js/main.98475a0a.chunk.js"
+  },
+  {
+    "revision": "a46000f4b7dabbe0d905",
+    "url": "/static/js/runtime-main.433e8715.js"
+  }
+]);
